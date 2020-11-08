738392d3c0dee39c031917d5de5d0ee9a510ca44
Viktoryia A <alekseevavikt@gmail.com>
2020-11-07 16:43:05 -0800

    added base url variable

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,5 @@
+let baseUrl = 'https://the-internet.herokuapp.com';
+
 exports.config = {
     //
     // ====================
@@ -90,7 +92,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'http://localhost',
+    baseUrl: baseUrl,
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
